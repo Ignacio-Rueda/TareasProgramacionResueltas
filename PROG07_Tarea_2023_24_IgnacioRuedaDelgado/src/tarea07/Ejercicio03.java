4b5dcc74bf954e5d037bbafd41de65d304e47bf5
Ignacio-Rueda <nachord@outlook.com>
2024-05-05 12:12:06 +0200

    Para que los elementos queden ordenados utiliza un TREESET

--- PROG07_Tarea_2023_24_IgnacioRuedaDelgado/src/tarea07/Ejercicio03.java
+++ PROG07_Tarea_2023_24_IgnacioRuedaDelgado/src/tarea07/Ejercicio03.java
@@ -1,43 +1,57 @@
 package tarea07;
 
-/** Ejercicio 3. Calendario de especies de plantas
- * @author Profesor
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * Ejercicio 3. Calendario de especies de plantas
+ *
+ * @author Ignacio
  */
 public class Ejercicio03 {
 
     public static void main(String[] args) {
-        
+
         //----------------------------------------------
         //    Declaración de variables y constantes
         //----------------------------------------------
-        
         // Constantes
-        
+        final int DIAS_SEMANA = 7;
         // Variables de entrada
-        
+
         // Variables auxiliares
-        
+        LocalDate fechaActual = LocalDate.now().minusDays(1);
+        DateTimeFormatter formatoFecha = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        int contadorDias = 1;
+        Map<LocalDate, String> calendario = new TreeMap<>();
         // Variables de salida
-        
 
         //----------------------------------------------
         //               Entrada de datos 
         //----------------------------------------------
-        
         // No se piden datos al usuario, ya que se usa un número fijo de elementos aleatorios
-        
         System.out.println("CALENDARIO DE ESPECIES DE PLANTAS");
         System.out.println("---------------------------------");
-        
+
         //----------------------------------------------
         //                  Procesamiento
         //----------------------------------------------
-        
-   
+        while(contadorDias<DIAS_SEMANA){
+            String planta = Utilidades.especiePlantaAleatoria();
+            if (!calendario.containsValue(planta)) {
+                calendario.put(fechaActual.plusDays(contadorDias), planta);
+                contadorDias++;
+            }
+
+        }
         //----------------------------------------------
         //           Salida de resultados
         //----------------------------------------------
-        
- 
+        for (Map.Entry<LocalDate, String> pos : calendario.entrySet()) {
+            System.out.printf("Fecha "+pos.getKey().format(formatoFecha) + ": " + pos.getValue() + "\n");
+        }
+
     }
 }
