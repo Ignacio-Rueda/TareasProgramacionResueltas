cda4d801b8760aaf059b1ad7a36fb670a25a3271
Ignacio Rueda <nachord@outlook.com>
2024-05-13 22:48:33 +0200

    Serializar y deserializar objetos

--- PROG08_Tarea2023_24_IgnacioRuedaDelgado/src/ejercicio2/RecetarioIO.java
+++ PROG08_Tarea2023_24_IgnacioRuedaDelgado/src/ejercicio2/RecetarioIO.java
@@ -1,5 +1,16 @@
 package ejercicio2;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.util.List;
+
 /**
  * Clase encargada de realizar la lectura y escritura de objetos Recetario en archivos binarios.
  * @author profe
@@ -27,7 +38,20 @@ public class RecetarioIO {
      * @return Objeto Recetario que estaba almacenado en el archivo binario.
      */
     public Recetario leer() {
-        return null; // Sustituir este return por el código que resuelve el ejercicio 
+        Recetario recetario = null;
+        try(ObjectInputStream entrada = new ObjectInputStream(new FileInputStream(rutaArchivo))){
+           recetario =(Recetario)entrada.readObject();
+        }catch(ClassNotFoundException ex){
+            System.out.println(ex.getMessage());
+        }
+        catch(FileNotFoundException ex){
+            System.out.println(ex.getMessage());
+        }catch(IOException ex){
+            System.out.println(ex.getMessage());
+        }
+        
+        
+        return recetario; // Sustituir este return por el código que resuelve el ejercicio 
     }
     
     /**
@@ -36,5 +60,15 @@ public class RecetarioIO {
      */   
     public void escribir(Recetario recetario) {
         // Incluir el código que debe realizar el método
+        File fichero = new File(rutaArchivo);
+        try(ObjectOutputStream escribir = new ObjectOutputStream(new FileOutputStream(fichero))){
+            escribir.writeObject(recetario);
+            escribir.flush();
+           
+        }catch(FileNotFoundException ex){
+            System.out.println(ex.getMessage());
+        }catch(IOException ex){
+            System.out.println(ex.getMessage());
+        }
     }
 }
